d94f721dab8cfa80ff3d8fa5fc03106868b08a2f
Nikhil <nperi104@uottawa.ca>
2018-12-02 10:34:07 -0500

    next controller

--- lib/controller.py
+++ lib/controller.py
@@ -8,32 +8,57 @@ from numpy import random
 from threading import Thread
 from datetime import datetime
 
+import routines.kinect as kinect_routine
 from routines import list_routines
 from lib.logger import configure_logger
 from lib.constants import DAYS_OF_WEEK
 from lib.wall import Wall
+from lib.kinect import Kinect
+from gpiozero import Button, LED
 
 DEFAULT_COM_PORT='/dev/ttyUSB0'
+ROUTINE_BUTTON_PIN=23
+ROUTINE_LED_PIN=24
 DEFAULT_TIMEOUT=15
 
 # Controller for the wall
 class Controller(Thread):
     def __init__(self, com_port, intervals_file='config/intervals.yml'):
+        self.active = False
         configure_logger()
         self.load_intervals(intervals_file)
         self.connect_comm(com_port)
+
+        self.routine_interrupt = False
+        time.sleep(10)
+        try:
+            self.routine_indicator = LED(ROUTINE_LED_PIN)
+            self.routine_indicator.on()
+            time.sleep(5)
+            self.routine_indicator.off()
+            self.routine_button = Button(ROUTINE_BUTTON_PIN)
+
+            def _set_interrupt():
+                self.routine_interrupt = True
+                self.routine_indicator.on()
+
+            self.routine_button.when_pressed = _set_interrupt
+        except Exception as e:
+            logging.error('Could not connect to GPIO')
+
         self.routines = list_routines()
         self.wall = Wall(self.comm)
-        self.active = False
         super(Controller, self).__init__()
 
     # Run specific wall routine (pattern)
     def run_routine(self, routine_name):
         try:
+            self.routine_indicator.on()
             self.routines[routine_name].run(self.wall)
             self.wall.reset()
             time.sleep(5)
-        except Exception as e:
+            self.routine_indicator.off()
+        except (WallInterrupt, Exception) as e:
             logging.error('Routine "{}" failed'.format(routine_name))
             logging.error(e)
 
@@ -41,6 +66,7 @@ class Controller(Thread):
     # Activate the wall
     def activate(self):
         if not self.active:
+            super(Controller, self).__init__()
             self.active = True
             self.start()
             if not self.is_alive():
@@ -49,7 +75,6 @@ class Controller(Thread):
     # Deactivate the wall
     def deactivate(self):
         if self.active:
-            super(Controller, self).__init__()
             self.active = False
             self.join()
             if self.is_alive():
@@ -57,19 +82,9 @@ class Controller(Thread):
 
     def run(self):
         while self.active:
-            if self.valid_run_interval():
+            if self.routine_interrupt:
+                self.routine_interrupt = False
                 self.run_routine(random.choice(self.routines.keys()))
-            else:
-                time.sleep(DEFAULT_TIMEOUT)
-
-
-    def valid_run_interval(self):
-        current_time = datetime.now()
-        intervals = self.intervals[DAYS_OF_WEEK[current_time.weekday()]]
-        for interval in intervals:
-            if interval['start'] < current_time.time() and current_time.time() < interval['stop']:
-                return True
-        return False
 
     # Connect to COM port in order to run
     def connect_comm(self, com_port, retry=5, timeout=DEFAULT_TIMEOUT):
@@ -90,7 +105,6 @@ class Controller(Thread):
             interval_config = yaml.load(file)
             for date, intervals in interval_config.items():
                 self.intervals[date] = [ {'start': datetime.strptime(i['start'], '%H:%M').time(), 'stop': datetime.strptime(i['stop'], '%H:%M').time()} for i in intervals ]
-
 
 if __name__ == '__main__':
     com_port = DEFAULT_COM_PORT
